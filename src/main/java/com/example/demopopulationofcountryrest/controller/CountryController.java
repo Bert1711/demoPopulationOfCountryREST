1112cbab26f929284292cd05ea2aeb9415312b2c
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-06 21:30:25 +0300

    "add ResponseEntity"

--- src/main/java/com/example/demopopulationofcountryrest/controller/CountryController.java
+++ src/main/java/com/example/demopopulationofcountryrest/controller/CountryController.java
@@ -1,14 +1,21 @@
 package com.example.demopopulationofcountryrest.controller;
 
 import com.example.demopopulationofcountryrest.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CountryController {
     @GetMapping("/france")
-    public Country france() {
+    public ResponseEntity<Country> france() {
         Country country = Country.of("France", 67);
-        return country;
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .header("favorite_food", "cheese and wine")
+                .body(country);
     }
 }
