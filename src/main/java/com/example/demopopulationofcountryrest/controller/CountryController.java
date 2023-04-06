ccbe09e57862b651dc00a086581e0836fc807809
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-06 21:57:24 +0300

    "change CountryController"

--- src/main/java/com/example/demopopulationofcountryrest/controller/CountryController.java
+++ src/main/java/com/example/demopopulationofcountryrest/controller/CountryController.java
@@ -8,14 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CountryController {
+
+    @GetMapping("/russia")
+    public ResponseEntity<Country> russia() {
+        Country country = Country.of("Russia", 148);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Continent", "Europe and Asia")
+                .header("Capital", "Moscow")
+                .header("Favorite-food", "dumplings")
+                .body(country);
+    }
     @GetMapping("/france")
     public ResponseEntity<Country> france() {
         Country country = Country.of("France", 67);
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
-                .header("continent", "Europe")
-                .header("capital", "Paris")
-                .header("favorite_food", "cheese and wine")
+                .header("Continent", "Europe")
+                .header("Capital", "Paris")
+                .header("Favorite-food", "cheese and wine")
                 .body(country);
     }
+
 }
