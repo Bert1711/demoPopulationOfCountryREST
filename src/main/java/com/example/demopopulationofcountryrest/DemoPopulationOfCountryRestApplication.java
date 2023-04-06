3a3e1af050e6010ea524594943b6d091ebf24b92
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-06 20:57:43 +0300

    "add Country class"

---
+++ src/main/java/com/example/demopopulationofcountryrest/DemoPopulationOfCountryRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopopulationofcountryrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPopulationOfCountryRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPopulationOfCountryRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demopopulationofcountryrest/DemoPopulationOfCountryRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopopulationofcountryrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPopulationOfCountryRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
